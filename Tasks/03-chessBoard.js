1d9d1233805ed54d7ddda3179a1d4af6e02f258f
choipongp <chotipong.p@mail.kmutt.ac.th>
2018-08-20 00:57:20 +0700

    'day2-100DaysChallenge'

---
+++ Tasks/03-chessBoard.js
@@ -0,0 +1,15 @@
+let size = 8;
+
+let output = "";
+
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    if ((i + j) % 2 == 0) {
+      output += " ";
+    } else {
+      output += "#";
+    }
+  }
+  output += "\n";
+}
+console.log(output);

d27c152d572ebb3936b81332911cadb80c772ff2
choipongp <chotipong.p@mail.kmutt.ac.th>
2018-08-27 23:35:22 +0700

    day7 of 100DaysChallenge

--- Tasks/12-deepComparison.js
+++ Tasks/12-deepComparison.js
@@ -1,22 +1,22 @@
 function deepEqual(a, b) {
-  if (a === b) return true;
+  if (a === b) return true
 
-  if (a == null || typeof a != "object" || b == null || typeof b != "object")
-    return false;
+  if (a == null || typeof a != 'object' || b == null || typeof b != 'object')
+    return false
 
   let keysA = Object.keys(a),
-    keysB = Object.keys(b);
+    keysB = Object.keys(b)
 
-  if (keysA.length != keysB.length) return false;
+  if (keysA.length != keysB.length) return false
 
   for (let key of keysA) {
-    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false
   }
 
-  return true;
+  return true
 }
 
-let obj = { here: { is: "an" }, object: 2 };
-console.log(deepEqual(obj, obj));
-console.log(deepEqual(obj, { here: 1, object: 2 }));
-console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
+let obj = { here: { is: 'an' }, object: 2 }
+console.log(deepEqual(obj, obj))
+console.log(deepEqual(obj, { here: 1, object: 2 }))
+console.log(deepEqual(obj, { here: { is: 'an' }, object: 2 }))

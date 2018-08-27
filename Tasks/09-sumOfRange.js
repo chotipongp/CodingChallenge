0bda0ea76a0695b5f6c9e1e5eb57d92b4fe00613
choipongp <chotipong.p@mail.kmutt.ac.th>
2018-08-27 23:34:51 +0700

    day4 of 100DaysChallenge

--- Tasks/09-sumOfRange.js
+++ Tasks/09-sumOfRange.js
@@ -1,22 +1,22 @@
 function range(start, end, step = start < end ? 1 : -1) {
-  let array = [];
+  let array = []
 
   if (step > 0) {
-    for (let i = start; i <= end; i += step) array.push(i);
+    for (let i = start; i <= end; i += step) array.push(i)
   } else {
-    for (let i = start; i >= end; i += step) array.push(i);
+    for (let i = start; i >= end; i += step) array.push(i)
   }
-  return array;
+  return array
 }
 
 function sum(array) {
-  let total = 0;
+  let total = 0
   for (let value of array) {
-    total += value;
+    total += value
   }
-  return total;
+  return total
 }
 
-console.log(range(1, 10));
-console.log(range(5, 2, -1));
-console.log(sum(range(1, 10)));
+console.log(range(1, 10))
+console.log(range(5, 2, -1))
+console.log(sum(range(1, 10)))

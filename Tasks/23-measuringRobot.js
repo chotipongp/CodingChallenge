9e58760b7ff271957e5c26e23129916b44d09924
choipongp <chotipong.p@mail.kmutt.ac.th>
2018-09-03 23:55:35 +0700

    Day17 of 100DaysChallenge

--- Tasks/23-measuringRobot.js
+++ Tasks/23-measuringRobot.js
@@ -10,3 +10,7 @@ console.log(/'\d+'/.test("'123'")) //true
 console.log(/'\d+'/.test("''")) // false
 console.log(/'\d*'/.test("'123'")) //true
 console.log(/'\d*'/.test("''")) //true
+
+let neighbor = /neighbou?r/
+console.log(neighbor.test('neighbour')) //true
+console.log(neighbor.test('neighbor')) //true

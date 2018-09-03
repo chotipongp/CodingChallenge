1e66020e3696a1ecc7eccf2300e47dc87b2cb2b1
choipongp <chotipong.p@mail.kmutt.ac.th>
2018-09-03 23:52:01 +0700

    Day17 of 100DaysChallenge

--- Tasks/23-measuringRobot.js
+++ Tasks/23-measuringRobot.js
@@ -1,3 +1,7 @@
 let dateTime = /\d\d-\d\d-\d\d\d\d \d\d:\d\d/
-console.log(dateTime.test('30-01-2003 15:20'))
-console.log(dateTime.test('30-jan-2013 15:11'))
+console.log(dateTime.test('30-01-2003 15:20')) //true
+console.log(dateTime.test('30-jan-2013 15:11')) //false
+
+let notBinary = /[^01]/
+console.log(notBinary.test('1101011010110')) //false
+console.log(notBinary.test('1101011010112')) //true
